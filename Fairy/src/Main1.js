bfe85c5da0d6d6e4791aec8b6a1db55effa491a7
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2022-03-21 13:13:25 +0100

    FairyTales1.1

--- Fairy/src/Main1.js
+++ Fairy/src/Main1.js
@@ -13,7 +13,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 500 },
-            debug: true,
+            debug: false ,
         },
     },
     scene: new TableauTiled()
